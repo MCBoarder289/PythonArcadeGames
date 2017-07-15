997fe7bcf9ff62c819b98df5f1c8cbe99e3014d7
Michael Chapman <chapman.michael.c@gmail.com>
2017-07-15 13:33:02 -0500

    Starting Chapter 5

--- Chapter 5 - Graphics/scratchwork.py
+++ Chapter 5 - Graphics/scratchwork.py
@@ -28,26 +28,38 @@ done = False  # This is the loop control
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()  # Control for how fast game runs
 
-# --------------- Main Program Loop -------------------- #
+# --------------- Main Program Loop --------------- #
 
 while not done:
+
+    # ---------- Main Event Loop ---------- #
     # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close note, this is not pygame.quit() which is a function
+            print("User asked to quit.")
             done = True  # Flag that we are done, so we exit this loop
-
-    # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
+        '''
+        elif event.type == pygame.KEYDOWN:
+            print("User pressed a key.")
+        elif event.type == pygame.KEYUP:
+            print("User let go of a key.")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print("User pressed a mouse button")
+        '''
+    # ALL EVENT PROCESSING SHOULD GO ABOVE THIS COMMENT
 
     # ALL GAME LOGIC SHOULD GO BELOW THIS COMMENT
 
     # ALL GAME LOGIC SHOULD GO ABOVE THIS COMMENT
 
     # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
-
+    screen.fill(WHITE)
+    pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
+    pygame.draw.line(screen, RED, [50, 0], [50, 50], 2)
     # ALL CODE TO DRAW SHOULD BE ABOVE THIS COMMENT
-
+    pygame.display.flip()
     # Limit to 20 frames per second
-    clock.tick(20)
+    clock.tick(60)
 
 pygame.quit()
 
